d93ec8cd980b03164923c5613bde8f16b350f427
Subhrajit Dey <subhrajitdey23@gmail.com>
2026-05-28 21:22:21 +0530

    Added new print in main.py

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ def hello_world():
     """Print a greeting message."""
     print("Hello from Git Demo Scenario 2!")
     print("This is from feature branch 1")
+    print("New feature2 Branch")
 
 
 
